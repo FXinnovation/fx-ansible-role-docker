d84d0bec322b32a6798d32f47502e11fd6907842
Julien Cabillot <julien.cabillot@fxinnovation.com>
2019-05-07 11:20:48 -0400

    Tweak tests

--- test/integration/default/centos.rb
+++ test/integration/default/centos.rb
@@ -1,7 +1,7 @@
 control "ansible-role-docker - #{os.name} #{os.release} - 01" do
   impact 1.0
   title 'Ansible role docker'
-  describe directory('/') do
-    it { should exist }
+  describe package('docker-ce') do
+    it { should be_installed }
   end
 end
